e7f207949dbd2666e18eab3e9a7e7860a3e81080
lbenav8095 <lubensaico@gmail.com>
2024-04-25 00:45:00 -0600

    Initial commit

---
+++ src/main/java/me/anjoismysign/itemstack/ItemStackBuilder.java
@@ -0,0 +1,348 @@
+package me.anjoismysign.itemstack;
+
+import me.anjoismysign.TextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemRarity;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.*;
+import org.bukkit.inventory.meta.components.FoodComponent;
+import org.bukkit.inventory.meta.trim.ArmorTrim;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+
+public final class ItemStackBuilder {
+    private final ItemStack itemStack;
+
+    private static final ItemFlag[] ALL_CONSTANTS = ItemFlag.values();
+
+    public static ItemStackBuilder build(ItemStack itemStack) {
+        return new ItemStackBuilder(itemStack.clone()).hideAll();
+    }
+
+    public static ItemStackBuilder build(Material material) {
+        return build(material, 1);
+    }
+
+    public static ItemStackBuilder build(Material material, int amount) {
+        return build(new ItemStack(material, amount));
+    }
+
+    public static ItemStackBuilder build(String material) {
+        return build(material, 1);
+    }
+
+    public static ItemStackBuilder build(String material, int amount) {
+        Material mat;
+        try {
+            mat = Material.valueOf(material);
+        } catch (IllegalArgumentException e) {
+            mat = Material.DIRT;
+            Bukkit.getLogger().info("Material " + material + " is not a valid material. Using DIRT instead.");
+        }
+        return build(mat, amount);
+    }
+
+    private ItemStackBuilder(ItemStack itemStack) {
+        this.itemStack = Objects.requireNonNull(itemStack, "itemStack cannot be null");
+    }
+
+    private ItemStackBuilder damageable(Consumer<Damageable> consumer){
+        ItemMeta m = this.itemStack.getItemMeta();
+        if (m instanceof Damageable damageable) {
+            consumer.accept(damageable);
+            this.itemStack.setItemMeta(m);
+        }
+        return this;
+    }
+
+    public ItemStackBuilder damage(int damage){
+        return damageable(damageable -> damageable.setDamage(damage));
+    }
+
+    public ItemStackBuilder maxDamage(int maxDamage){
+        return damageable(damageable -> damageable.setMaxDamage(maxDamage));
+    }
+
+    private ItemStackBuilder repariable(Consumer<Repairable> consumer){
+        ItemMeta m = this.itemStack.getItemMeta();
+        if (m instanceof Repairable repairable) {
+            consumer.accept(repairable);
+            this.itemStack.setItemMeta(m);
+        }
+        return this;
+    }
+
+    public ItemStackBuilder repairCost(int repairCost){
+        return repariable(repairable -> repairable.setRepairCost(repairCost));
+    }
+
+    private ItemStackBuilder armor(Consumer<ArmorMeta> consumer){
+        ItemMeta m = this.itemStack.getItemMeta();
+        if (m instanceof ArmorMeta armorMeta) {
+            consumer.accept(armorMeta);
+            this.itemStack.setItemMeta(m);
+        }
+        return this;
+    }
+
+    public ItemStackBuilder armorTrim(ArmorTrim armorTrim){
+        return armor(armorMeta -> armorMeta.setTrim(armorTrim));
+    }
+
+    private ItemStackBuilder itemMeta(Consumer<ItemMeta> consumer) {
+        ItemMeta m = this.itemStack.getItemMeta();
+        if (m != null) {
+            consumer.accept(m);
+            this.itemStack.setItemMeta(m);
+        }
+        return this;
+    }
+
+    public ItemStackBuilder itemName(String name){
+        return itemMeta(itemMeta -> itemMeta.setItemName(name));
+    }
+
+    public ItemStackBuilder hideToolTip(boolean hideToolTip){
+        return itemMeta(itemMeta -> itemMeta.setHideTooltip(hideToolTip));
+    }
+
+    public ItemStackBuilder enchantmentGlintOverride(boolean glint){
+        return itemMeta(itemMeta -> itemMeta.setEnchantmentGlintOverride(glint));
+    }
+
+    public ItemStackBuilder fireResistant(boolean fireResistant){
+        return itemMeta(itemMeta -> itemMeta.setFireResistant(fireResistant));
+    }
+
+    public ItemStackBuilder maxStackSize(int maxStackSize){
+        return itemMeta(itemMeta -> itemMeta.setMaxStackSize(maxStackSize));
+    }
+
+    public ItemStackBuilder rarity(ItemRarity rarity){
+        return itemMeta(itemMeta -> itemMeta.setRarity(rarity));
+    }
+
+    public ItemStackBuilder food(Consumer<FoodComponent> consumer){
+        return itemMeta(itemMeta -> {
+            FoodComponent foodComponent = itemMeta.getFood();
+            consumer.accept(foodComponent);
+            itemMeta.setFood(foodComponent);
+        });
+    }
+
+    public ItemStackBuilder hideAll() {
+        return flag(ALL_CONSTANTS);
+    }
+
+    public ItemStackBuilder showAll() {
+        return unflag(ALL_CONSTANTS);
+    }
+
+    public ItemStackBuilder flag(ItemFlag... flags) {
+        return itemMeta(itemMeta -> itemMeta.addItemFlags(flags));
+    }
+
+    public ItemStackBuilder flag(Collection<ItemFlag> flags) {
+        return flag(flags.toArray(new ItemFlag[0]));
+    }
+
+    public ItemStackBuilder deserializeAndFlag(List<String> flags) {
+        List<ItemFlag> itemFlags = new ArrayList<>();
+        for (String flag : flags) {
+            try {
+                itemFlags.add(ItemFlag.valueOf(flag));
+            } catch (IllegalArgumentException e) {
+                Bukkit.getLogger().info("ItemFlag " + flag + " is not a valid ItemFlag.");
+            }
+        }
+        return flag(itemFlags);
+    }
+
+    public ItemStackBuilder unflag(ItemFlag... flags) {
+        return itemMeta(itemMeta -> itemMeta.removeItemFlags(flags));
+    }
+
+    public ItemStackBuilder unflag(Collection<ItemFlag> flags) {
+        return unflag(flags.toArray(new ItemFlag[0]));
+    }
+
+    public ItemStackBuilder displayName(String name, char colorChar) {
+        return itemMeta(itemMeta -> itemMeta.setDisplayName(TextColor.CUSTOM_PARSE(colorChar, name)));
+    }
+
+    public ItemStackBuilder displayName(String name) {
+        return displayName(name, '&');
+    }
+
+    public ItemStackBuilder lore(String line, char colorChar) {
+        return lore(List.of(line), colorChar);
+    }
+
+    public ItemStackBuilder lore(String line) {
+        return lore(line, '&');
+    }
+
+    public ItemStackBuilder lore(char colorChar, String... lore) {
+        List<String> list = List.of(lore);
+        List<String> dupe = new ArrayList<>();
+        list.forEach(s -> dupe.add(TextColor.CUSTOM_PARSE(colorChar, s)));
+        return itemMeta(itemMeta -> itemMeta.setLore(dupe));
+    }
+
+    public ItemStackBuilder lore(String... lore) {
+        return lore('&', lore);
+    }
+
+    public ItemStackBuilder lore(List<String> lore, char colorChar) {
+        List<String> list = lore.stream().map(s -> TextColor.CUSTOM_PARSE(colorChar, s)).toList();
+        return itemMeta(itemMeta -> itemMeta.setLore(list));
+    }
+
+    public ItemStackBuilder lore(List<String> lore) {
+        return lore(lore, '&');
+    }
+
+    private List<String> getLore() {
+        ItemMeta meta = itemStack.getItemMeta();
+        if (meta == null) return null;
+        return meta.getLore();
+    }
+
+    /**
+     * Clears the lore of the item.
+     */
+    public ItemStackBuilder lore() {
+        return itemMeta(itemMeta -> itemMeta.setLore(new ArrayList<>()));
+    }
+
+    public ItemStackBuilder customModelData(int amount) {
+        return itemMeta(itemMeta -> itemMeta.setCustomModelData(amount));
+    }
+
+    private ItemStackBuilder itemStack(Consumer<ItemStack> consumer) {
+        consumer.accept(this.itemStack);
+        return this;
+    }
+
+    public ItemStackBuilder amount(int amount) {
+        return itemStack(itemStack -> itemStack.setAmount(amount));
+    }
+
+    public ItemStackBuilder type(Material material) {
+        return itemStack(itemStack -> itemStack.setType(material));
+    }
+
+    public ItemStackBuilder enchant(Enchantment enchantment, int level) {
+        return itemStack(itemStack -> itemStack.addUnsafeEnchantment(enchantment, level));
+    }
+
+    public ItemStackBuilder unenchant(Enchantment enchantment) {
+        return itemStack(itemStack -> itemStack.removeEnchantment(enchantment));
+    }
+
+    public ItemStackBuilder enchant(Enchantment... enchantments) {
+        return itemStack(itemStack -> {
+            for (Enchantment enchantment : enchantments) {
+                itemStack.addUnsafeEnchantment(enchantment, 1);
+            }
+        });
+    }
+
+    public ItemStackBuilder enchant(Collection<Enchantment> enchantments) {
+        return itemStack(itemStack -> {
+            for (Enchantment enchantment : enchantments) {
+                itemStack.addUnsafeEnchantment(enchantment, 1);
+            }
+        });
+    }
+
+    public ItemStackBuilder enchant(Map<Enchantment, Integer> enchantments) {
+        return itemStack(itemStack -> {
+            for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
+                itemStack.addUnsafeEnchantment(entry.getKey(), entry.getValue());
+            }
+        });
+    }
+
+    public ItemStackBuilder deserializeAndEnchant(Collection<String> serializedEnchantments) {
+        HashMap<Enchantment, Integer> enchantments = new HashMap<>();
+        serializedEnchantments.forEach(element -> {
+            String[] split = element.split(",");
+            if (split.length != 2) {
+                Bukkit.getLogger().severe("Invalid element inside 'Enchantments': " + element);
+                return;
+            }
+            String key = split[0];
+            int level;
+            try {
+                level = Integer.parseInt(split[1]);
+            } catch (NumberFormatException e) {
+                Bukkit.getLogger().severe("Invalid level for " + key + " enchantment: " + split[1]);
+                return;
+            }
+            Enchantment enchantment = Enchantment.getByKey(NamespacedKey.minecraft(key));
+            if (enchantment != null) {
+                enchantments.put(enchantment, level);
+            } else {
+                Bukkit.getLogger().severe("Enchantment " + key + " is not a valid enchantment. Skipping.");
+            }
+        });
+        return enchant(enchantments);
+    }
+
+    public ItemStackBuilder unenchant(Enchantment... enchantments) {
+        return itemStack(itemStack -> {
+            for (Enchantment enchantment : enchantments) {
+                itemStack.removeEnchantment(enchantment);
+            }
+        });
+    }
+
+    public ItemStackBuilder enchant(Enchantment enchantment) {
+        return enchant(enchantment, 1);
+    }
+
+    public ItemStackBuilder clearEnchantments() {
+        return itemMeta(itemStack -> itemStack.getEnchants().keySet().forEach(itemStack::removeEnchant));
+    }
+
+    public ItemStackBuilder color(Color color) {
+        return itemStack(itemStack -> {
+            Material type = itemStack.getType();
+            if (type == Material.LEATHER_BOOTS || type == Material.LEATHER_CHESTPLATE || type == Material.LEATHER_HELMET || type == Material.LEATHER_LEGGINGS) {
+                LeatherArmorMeta meta = (LeatherArmorMeta) itemStack.getItemMeta();
+                meta.setColor(color);
+                itemStack.setItemMeta(meta);
+            }
+        });
+    }
+
+    public ItemStackBuilder attribute(Attribute attribute, double amount, AttributeModifier.Operation operation) {
+        return itemMeta(itemMeta -> {
+            try {
+                UUID uuid = UUID.randomUUID();
+                AttributeModifier modifier = new AttributeModifier(uuid, uuid.toString(), amount, operation);
+                itemMeta.addAttributeModifier(attribute, modifier);
+            } catch (Exception exception) {
+                Bukkit.getLogger().log(Level.SEVERE, exception, () -> "Failed to add attribute modifier");
+            }
+        });
+    }
+
+    public ItemStackBuilder unbreakable(boolean unbreakable) {
+        return itemMeta(itemMeta -> itemMeta.setUnbreakable(unbreakable));
+    }
+
+    public ItemStack build() {
+        return this.itemStack;
+    }
+}
---
+++ src/main/java/me/anjoismysign/itemstack/ItemStackReader.java
@@ -0,0 +1,252 @@
+package me.anjoismysign.itemstack;
+
+import me.anjoismysign.SkullCreator;
+import me.anjoismysign.TextColor;
+import me.anjoismysign.anjo.entities.Uber;
+import me.anjoismysign.exception.ConfigurationFieldException;
+import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.Registry;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemRarity;
+import org.bukkit.inventory.meta.trim.ArmorTrim;
+import org.bukkit.inventory.meta.trim.TrimMaterial;
+import org.bukkit.inventory.meta.trim.TrimPattern;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ItemStackReader {
+
+    public static ItemStackBuilder READ_OR_FAIL_FAST(ConfigurationSection section) {
+        if (!section.isString("Material"))
+            throw new ConfigurationFieldException("'Material' field is missing or not a String");
+        String inputMaterial = section.getString("Material");
+        ItemStackBuilder builder;
+        if (!inputMaterial.startsWith("HEAD-")) {
+            Material material = Material.getMaterial(inputMaterial);
+            if (material == null)
+                throw new ConfigurationFieldException("'Material' field is not a valid material");
+            builder = ItemStackBuilder.build(material);
+        } else
+            builder = ItemStackBuilder.build(SkullCreator.itemFromUrl(inputMaterial.substring(5)));
+        if (section.isInt("Amount")) {
+            int amount = section.getInt("Amount");
+            if (amount < 1 || amount > 127)
+                throw new ConfigurationFieldException("'Amount' field is not a valid amount");
+            builder = builder.amount(section.getInt("Amount"));
+        }
+        if (section.isInt("Damage")) {
+            int damage = section.getInt("Damage");
+            if (damage < 0)
+                throw new ConfigurationFieldException("'Damage' field is not a valid damage value");
+            builder = builder.damage(damage);
+        }
+        if (section.isInt("MaxDamage")) {
+            int maxDamage = section.getInt("MaxDamage");
+            if (maxDamage < 0)
+                throw new ConfigurationFieldException("'MaxDamage' field is not a valid max damage value");
+            builder = builder.maxDamage(maxDamage);
+        }
+        if (section.isInt("RepairCost")) {
+            int repairCost = section.getInt("RepairCost");
+            if (repairCost < 0)
+                throw new ConfigurationFieldException("'RepairCost' field is not a valid repair cost value");
+            builder = builder.repairCost(repairCost);
+        }
+        if (section.isConfigurationSection("ArmorTrim")){
+            ConfigurationSection armorTrim = section.getConfigurationSection("ArmorTrim");
+            if (!armorTrim.isString("TrimMaterial"))
+                throw new ConfigurationFieldException("ArmorTrim is missing 'TrimMaterial' field");
+            String trimMaterial = armorTrim.getString("TrimMaterial");
+            if (!armorTrim.isString("TrimPattern"))
+                throw new ConfigurationFieldException("ArmorTrim is missing 'TrimPattern' field");
+            String trimPattern = armorTrim.getString("TrimPattern");
+            TrimMaterial material = Registry.TRIM_MATERIAL.match(trimMaterial);
+            if (material == null)
+                throw new ConfigurationFieldException("'"+trimPattern + "' is not a valid TrimMaterial");
+            TrimPattern pattern = Registry.TRIM_PATTERN.match(trimPattern);
+            if (pattern == null)
+                throw new ConfigurationFieldException("'"+trimPattern + "' is not a valid TrimPattern");
+            builder = builder.armorTrim(new ArmorTrim(material, pattern));
+        }
+        if (section.isString("ItemName")){
+            String itemName = section.getString("ItemName");
+            builder = builder.itemName(itemName);
+        }
+        if (section.isBoolean("HideToolTip")){
+            boolean hideToolTip = section.getBoolean("HideToolTip");
+            builder = builder.hideToolTip(hideToolTip);
+        }
+        if (section.isBoolean("EnchantmentGlintOverride")){
+            boolean enchantmentGlintOverride = section.getBoolean("EnchantmentGlintOverride");
+            builder = builder.enchantmentGlintOverride(enchantmentGlintOverride);
+        }
+        if (section.isBoolean("FireResistant")){
+            boolean fireResistant = section.getBoolean("FireResistant");
+            builder = builder.fireResistant(fireResistant);
+        }
+        if (section.isInt("MaxStackSize")){
+            int maxStackSize = section.getInt("MaxStackSize");
+            builder = builder.maxStackSize(maxStackSize);
+        }
+        if (section.isString("Rarity")){
+            ItemRarity rarity = ItemRarity.valueOf(section.getString("Rarity"));
+            builder = builder.rarity(rarity);
+        }
+        if (section.isConfigurationSection("Food")){
+            ConfigurationSection food = section.getConfigurationSection("Food");
+            if (food.isInt("Nutrition"))
+                builder = builder.food(foodComponent -> foodComponent.setNutrition(food.getInt("Nutrition")));
+            if (food.isDouble("Saturation")){
+                float saturation = (float) food.getDouble("Saturation");
+                builder = builder.food(foodComponent -> foodComponent.setSaturation(saturation));
+            }
+            if (food.isBoolean("CanAlwaysEat"))
+                builder = builder.food(foodComponent -> foodComponent.setCanAlwaysEat(food.getBoolean("CanAlwaysEat")));
+            if (food.isDouble("EatSeconds")){
+                float eatSeconds = (float) food.getDouble("EatSeconds");
+                builder = builder.food(foodComponent -> foodComponent.setEatSeconds(eatSeconds));
+            }
+            if (food.isConfigurationSection("FoodEffects")){
+                ConfigurationSection outer = food.getConfigurationSection("FoodEffects");
+                for (String key : outer.getKeys(false)){
+                    ConfigurationSection foodEffect = outer.getConfigurationSection(key);
+                    if (!foodEffect.isDouble("Probability"))
+                        throw new ConfigurationFieldException("FoodEffect '" + key + "' is missing 'Probability' field");
+                    if (!foodEffect.isConfigurationSection("PotionEffect"))
+                        throw new ConfigurationFieldException("FoodEffect '" + key + "' is missing 'PotionEffect' field");
+                    ConfigurationSection potionEffect = foodEffect.getConfigurationSection("PotionEffect");
+                    if (!potionEffect.isString("Type"))
+                        throw new ConfigurationFieldException("PotionEffect '" + key + "' is missing 'Type' field");
+                    String type = potionEffect.getString("Type");
+                    PotionEffectType potionEffectType = Registry.EFFECT.match(type);
+                    if (potionEffectType == null)
+                        throw new ConfigurationFieldException("PotionEffect '" + key + "' doesn't point to a valid PotionEffectType");
+                    if (!potionEffect.isInt("Duration"))
+                        throw new ConfigurationFieldException("PotionEffect '" + key + "' is missing 'Duration' field");
+                    int duration = potionEffect.getInt("Duration");
+                    if (!potionEffect.isInt("Amplifier"))
+                        throw new ConfigurationFieldException("PotionEffect '" + key + "' is missing 'Amplifier' field");
+                    int amplifier = potionEffect.getInt("Amplifier");
+                    boolean ambient = potionEffect.getBoolean("Ambient", true);
+                    boolean particles = potionEffect.getBoolean("Particles", true);
+                    boolean icon = potionEffect.getBoolean("Icon", true);
+                    PotionEffect effect = new PotionEffect(potionEffectType, duration, amplifier, ambient, particles, icon);
+                    float probability = (float) foodEffect.getDouble("Probability");
+                    builder = builder.food(foodComponent -> foodComponent.addEffect(effect, probability));
+                }
+            }
+        }
+        if (section.isString("DisplayName")) {
+            builder = builder.displayName(TextColor.PARSE(section
+                    .getString("DisplayName")));
+        }
+        if (section.isList("Lore")) {
+            List<String> input = section.getStringList("Lore");
+            List<String> lore = new ArrayList<>();
+            input.forEach(string -> lore.add(TextColor.PARSE(string)));
+            builder = builder.lore(lore);
+        }
+        if (section.isBoolean("Unbreakable")) {
+            builder = builder.unbreakable(section.getBoolean("Unbreakable"));
+        }
+        if (section.isString("Color")) {
+            builder = builder.color(parseColor(section.getString("Color")));
+        }
+        if (section.isList("Enchantments")) {
+            List<String> enchantNames = section.getStringList("Enchantments");
+            builder = builder.deserializeAndEnchant(enchantNames);
+        }
+        if (section.isInt("CustomModelData")) {
+            builder = builder.customModelData(section.getInt("CustomModelData"));
+        }
+        boolean showAll = section.getBoolean("ShowAllItemFlags", false);
+        if (showAll)
+            builder = builder.showAll();
+        if (section.isList("ItemFlags")) {
+            List<String> flagNames = section.getStringList("ItemFlags");
+            builder = builder.deserializeAndFlag(flagNames);
+        }
+        if (section.isConfigurationSection("Attributes")) {
+            ConfigurationSection attributes = section.getConfigurationSection("Attributes");
+            Uber<ItemStackBuilder> uber = Uber.drive(builder);
+            attributes.getKeys(false).forEach(key -> {
+                if (!attributes.isConfigurationSection(key))
+                    throw new ConfigurationFieldException("Attribute '" + key + "' is not valid");
+                ConfigurationSection attributeSection = attributes.getConfigurationSection(key);
+                try {
+                    Attribute attribute = Attribute.valueOf(key);
+                    if (!attributeSection.isDouble("Amount"))
+                        throw new ConfigurationFieldException("Attribute '" + key + "' has an invalid amount (DECIMAL NUMBER)");
+                    double amount = attributeSection.getDouble("Amount");
+                    if (!attributeSection.isString("Operation"))
+                        throw new ConfigurationFieldException("Attribute '" + key + "' is missing 'Operation' field");
+                    AttributeModifier.Operation operation = AttributeModifier.Operation.valueOf(attributeSection.getString("Operation"));
+                    uber.talk(uber.thanks().attribute(attribute, amount, operation));
+                } catch (IllegalArgumentException e) {
+                    throw new ConfigurationFieldException("Attribute '" + key + "' has an invalid Operation");
+                }
+            });
+            builder = uber.thanks();
+        }
+        return builder;
+    }
+
+    @NotNull
+    public static ItemStackBuilder read(ConfigurationSection section) {
+        ItemStackBuilder builder;
+        try {
+            builder = READ_OR_FAIL_FAST(section);
+            return builder;
+        } catch (Exception e) {
+            Bukkit.getLogger().severe(e.getMessage());
+            return ItemStackBuilder.build(Material.DIRT);
+        }
+    }
+
+    public static ItemStackBuilder read(File file, String path) {
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        return READ_OR_FAIL_FAST(Objects.requireNonNull(config.getConfigurationSection(path)));
+    }
+
+    public static ItemStackBuilder read(File file) {
+        return read(file, "ItemStack");
+    }
+
+    public static ItemStackBuilder read(YamlConfiguration config, String path) {
+        return READ_OR_FAIL_FAST(Objects.requireNonNull(config.getConfigurationSection(path)));
+    }
+
+    public static ItemStackBuilder read(YamlConfiguration config) {
+        return read(config, "ItemStack");
+    }
+
+    public static Color parseColor(String color) {
+        String[] input = color.split(",");
+        if (input.length != 3) {
+            throw new IllegalArgumentException("Color " + color + " is not a valid color.");
+        }
+        try {
+            int r = Integer.parseInt(input[0]);
+            int g = Integer.parseInt(input[1]);
+            int b = Integer.parseInt(input[2]);
+            return Color.fromRGB(r, g, b);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Color " + color + " is not a valid color.");
+        }
+    }
+
+    public static String parse(Color color) {
+        return color.getRed() + "," + color.getGreen() + "," + color.getBlue();
+    }
+}
